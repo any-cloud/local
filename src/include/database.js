b1cdffede6cbf3c2d6350bde0c9e4403157a4078
Bryan Goldstein <brysgo@gmail.com>
2019-08-30 07:39:30 -0400

    add key search and did you mean for get

--- src/include/database.js
+++ src/include/database.js
@@ -4,7 +4,7 @@ const redis = new Redis();
 const ENCAPSULATION_CHECK = "qpj32r4wefeklnfaroiefeh";
 
 const unwrapKey = ({ key, encapsulation }) => {
-  if (!key || key === "") {
+  if (false && !key) {
     throw new Error("tried to use database with empty key");
   }
   if (!encapsulation || encapsulation !== ENCAPSULATION_CHECK) {
@@ -13,13 +13,15 @@ const unwrapKey = ({ key, encapsulation }) => {
   return key;
 };
 
+const keySep = ":";
+
 export const key = (...parts) => {
   parts.forEach(k => {
-    if (!key || key === "") {
+    if (false && !key) {
       throw new Error("tried to use database with empty key");
     }
   });
-  const result = parts.join(":");
+  const result = parts.join(keySep);
   return { key: result, encapsulation: ENCAPSULATION_CHECK };
 };
 
@@ -27,7 +29,38 @@ export const set = (key, value) => {
   return redis.set(unwrapKey(key), JSON.stringify(value));
 };
 
-export const get = key => redis.get(unwrapKey(key)).then(JSON.parse);
+export const get = async aKey => {
+  const result = await redis.get(unwrapKey(aKey));
+  if (!result) {
+    const namespace = unwrapKey(aKey).split(keySep);
+    let lastKeyPart = namespace.pop();
+    const didYouMean = await keySearch(`*${lastKeyPart}`);
+    if (didYouMean && didYouMean.length > 0) {
+      console.error(
+        `key not found in '${namespace}', did you mean: ${didYouMean}`
+      );
+    } else {
+      console.error(`key not found '${unwrapKey(aKey)}'`);
+    }
+  } else {
+    return JSON.parse(result);
+  }
+};
+
+export const keySearch = keyPattern => {
+  var stream = redis.scanStream({ match: keyPattern });
+  return new Promise((resolve, reject) => {
+    const result = [];
+
+    stream.on("data", resultKeys => {
+      for (var i = 0; i < resultKeys.length; i++) {
+        result.push(resultKeys[i]);
+      }
+    });
+    stream.on("end", () => resolve(Promise.all(result)));
+    stream.on("error", error => reject(error));
+  });
+};
 
 export const getAll = partialKey => {
   var stream = redis.scanStream({ match: `${unwrapKey(partialKey)}*` });
