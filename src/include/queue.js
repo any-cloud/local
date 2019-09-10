19c10e9fd42854ff3089a6f64d3c52fae9f1ada9
Bryan Goldstein <brysgo@gmail.com>
2019-09-10 15:27:59 -0400

    return promise from queue.push

--- src/include/queue.js
+++ src/include/queue.js
@@ -4,11 +4,11 @@ import path from "path";
 let onlyQueue;
 
 export const init = () => {
-    onlyQueue = new Queue("only");
-    onlyQueue.process(path.join(__dirname, "worker.js"));
-    return onlyQueue;
-}
+  onlyQueue = new Queue("only");
+  onlyQueue.process(path.join(__dirname, "worker.js"));
+  return onlyQueue;
+};
 
 export const push = (jobName, args) => {
-  onlyQueue.add({jobName, ...args});
+  return onlyQueue.add({ jobName, ...args });
 };
