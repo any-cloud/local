f168fc0bac9f5275eb7c87a411664e8c340a3b83
Bryan Goldstein <brysgo@gmail.com>
2019-09-19 10:08:44 -0400

    changes made to support new apis
    * database added getAllKeys
    * queue added runAll

--- src/include/database.js
+++ src/include/database.js
@@ -8,22 +8,40 @@ export const set = (key, value) => {
   return redis.set(common.unwrapKey(key), JSON.stringify(value));
 };
 
-export const get = async aKey => {
+export const get = async (aKey, opts = {}) => {
   const result = await redis.get(common.unwrapKey(aKey));
   if (!result) {
-    const namespace = common.unwrapKey(aKey).split(keySep);
+    if (opts.silent) return;
+    const keyString = common.unwrapKey(aKey);
+    const namespace = keyString.split(keySep);
     let lastKeyPart = namespace.pop();
     const didYouMean = await keySearch(`*${lastKeyPart}`);
     if (didYouMean && didYouMean.length > 0) {
       console.error(
-        `key not found in '${namespace}', did you mean: ${didYouMean}`
+        `key not found: '${keyString}', did you mean: ${didYouMean}`
       );
     } else {
       console.error(`key not found '${common.unwrapKey(aKey)}'`);
     }
+    console.trace();
   } else {
     return JSON.parse(result);
   }
+};
+
+export const getAllKeys = async partialKey => {
+  var stream = redis.scanStream({ match: `${common.unwrapKey(partialKey)}*` });
+  return new Promise((resolve, reject) => {
+    const result = [];
+
+    stream.on("data", resultKeys => {
+      for (var i = 0; i < resultKeys.length; i++) {
+        result.push(common.key(resultKeys[i]));
+      }
+    });
+    stream.on("end", () => resolve(result));
+    stream.on("error", error => reject(error));
+  });
 };
 
 export const getAll = partialKey => {
@@ -43,22 +61,13 @@ export const getAll = partialKey => {
 
 export const remove = key => redis.del(common.unwrapKey(key));
 
-export const reset = ({ force }) => {
+export const reset = async ({ force }) => {
   if (force) {
-    var stream = redis.scanStream({ match: "*" });
-    return new Promise((resolve, reject) => {
-      const result = [];
-
-      stream.on("data", resultKeys => {
-        for (var i = 0; i < resultKeys.length; i++) {
-          result.push(redis.del(resultKeys[i]));
-        }
-      });
-      stream.on("end", () => resolve(Promise.all(result)));
-      stream.on("error", error => reject(error));
-    });
+    await redis.flushall();
+    return true;
   } else {
     console.warn("refusing to reset database, force was not set");
+    return false;
   }
 };
 
--- src/include/queue.js
+++ src/include/queue.js
@@ -15,15 +15,36 @@ export const init = () => {
 
   // start cron jobs
   if (appCron) {
-    Object.keys(appCron).forEach(cronJobKey => {
-      const cronJob = appCron[cronJobKey];
-      onlyQueue.add({ jobName: cronJobKey }, { repeat: { cron: cronJob[0] } });
-    });
+    if (process.env.NODE_ENV === "test") {
+      console.log("skipping cron in test environment");
+    } else {
+      Object.keys(appCron).forEach(cronJobKey => {
+        const cronJob = appCron[cronJobKey];
+        onlyQueue.add(
+          { jobName: cronJobKey },
+          { repeat: { cron: cronJob[0] } }
+        );
+      });
+    }
   } else {
     console.log("no cron jobs found");
   }
 };
 
-export const push = (jobName, args) => {
-  return onlyQueue.add({ jobName, ...args });
+export const runAll = async () => {
+  await Promise.all(
+    (await onlyQueue.getJobs([
+      "waiting",
+      "active",
+      "completed",
+      "failed",
+      "delayed"
+    ])).map(j => j.finished())
+  );
+  return true;
+};
+
+export const push = async (jobName, args) => {
+  await onlyQueue.add({ jobName, ...args });
+  return true;
 };
